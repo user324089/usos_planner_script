c37f19c42a9ce59163fbaaadcd1ca450ef33f9ea
Ewa Kaluś <ewakalus1@gmail.com>
2024-08-05 22:43:21 +0200

    style changes

--- planner.py
+++ planner.py
@@ -14,11 +14,13 @@ from bs4 import BeautifulSoup
 import bs4
 
 NUM_PLANS = 3
+USOSWEB_KONTROLER = 'https://usosweb.mimuw.edu.pl/kontroler.php'
+DEFAULT_TIMEOUT = 10
 
 def read_words_from_file (filename: str) -> list[str]:
     """Returns a list of all words in the file."""
     try:
-        with open (filename, 'r') as f:
+        with open (filename, 'r', encoding="utf-8") as f:
             return f.read().split()
     except FileNotFoundError:
         return []
@@ -26,39 +28,50 @@ def read_words_from_file (filename: str) -> list[str]:
 def log_in_to_usos (username, password):
     """Logs into USOS and returns cookies with PHP session."""
 
-    r1 = requests.get('https://logowanie.uw.edu.pl/cas/login', timeout=20)
+    r1 = requests.get('https://logowanie.uw.edu.pl/cas/login', timeout=DEFAULT_TIMEOUT)
     cookies = r1.cookies
 
     lt = re.findall ('name="lt" value="(.*?)"', r1.text)[0]
     execution = re.findall ('name="execution" value="(.*?)"', r1.text)[0]
     event_id = re.findall ('name="_eventId" value="(.*?)"', r1.text)[0]
 
-    r2 = requests.post('https://logowanie.uw.edu.pl/cas/login',
-                       data= {'lt': lt, 'execution': execution, '_eventId': event_id,
-                              'username': username, 'password': password,
-                              'jsessionid': r1.cookies['JSESSIONID']},
-                       cookies=r1.cookies, timeout=20)
+    r2 = requests.post(
+        'https://logowanie.uw.edu.pl/cas/login',
+        data= {'lt': lt, 'execution': execution, '_eventId': event_id,
+               'username': username, 'password': password, 'jsessionid': r1.cookies['JSESSIONID']},
+        cookies=r1.cookies,
+        timeout=DEFAULT_TIMEOUT
+    )
     cookies.update(r2.cookies)
 
-    r3 = requests.get ('https://logowanie.uw.edu.pl/cas/login',
-                       params={'service': 'https://usosweb.mimuw.edu.pl/kontroler.php?_action=news/default',
-                               'gateway': 'true'},
-                       cookies=cookies, timeout=20)
+    r3 = requests.get(
+        'https://logowanie.uw.edu.pl/cas/login',
+        params={'service': USOSWEB_KONTROLER + '?_action=news/default',
+                'gateway': 'true'},
+        cookies=cookies,
+        timeout=DEFAULT_TIMEOUT
+    )
     print ('logged in to usos')
     return r3.cookies
 
 def add_course_to_plan(plan_id: int, course_code: str, dydactic_cycle: str, cookies):
     """Adds the course (all groups) to the plan."""
-    requests.get('https://usosweb.mimuw.edu.pl/kontroler.php',
-                 params={'_action': 'home/plany/dodajWpis', 'plan_id': plan_id,
-                         'klasa': 'P', 'prz_kod': course_code, 'cdyd_kod': dydactic_cycle},
-                 cookies=cookies, timeout=20)
+    requests.get(
+        USOSWEB_KONTROLER,
+        params={'_action': 'home/plany/dodajWpis', 'plan_id': plan_id,
+                'klasa': 'P', 'prz_kod': course_code, 'cdyd_kod': dydactic_cycle},
+        cookies=cookies,
+        timeout=DEFAULT_TIMEOUT
+    )
 
 def create_plan (name: str, cookies) -> int:
     """Creates an empty plan in USOS and returns its id."""
-    create_request = requests.get ('https://usosweb.mimuw.edu.pl/kontroler.php',
-                                   params={'_action': 'home/plany/utworz', 'nazwa': name},
-                                   cookies=cookies, timeout=20)
+    create_request = requests.get (
+        USOSWEB_KONTROLER,
+        params={'_action': 'home/plany/utworz', 'nazwa': name},
+        cookies=cookies,
+        timeout=DEFAULT_TIMEOUT
+    )
     print ('created plan:', name)
     return re.findall (r'plan_id=(\d*)', create_request.url)[0]
 
@@ -82,34 +95,45 @@ def get_csrf_token(string: str) -> str:
 
 def rename_plan (plan_id: int, new_name: str, cookies):
     """Changes plan name to new_name."""
-    edit_request = requests.get('https://usosweb.mimuw.edu.pl/kontroler.php',
-                                  params={'_action': 'home/plany/edytuj',
-                                          'plan_id': plan_id},
-                                  cookies=cookies, timeout=20)
-
+    edit_request = requests.get(
+        USOSWEB_KONTROLER,
+        params={'_action': 'home/plany/edytuj', 'plan_id': plan_id},
+        cookies=cookies,
+        timeout=DEFAULT_TIMEOUT
+    )
     csrftoken: str = get_csrf_token(edit_request.text)
-    payload, boundary = create_form_str({'_action': 'home/plany/zmienNazwe',
-                                         'plan_id': str(plan_id),
-                                         'csrftoken': csrftoken,
-                                         'nazwa': new_name})
-    requests.post('https://usosweb.mimuw.edu.pl/kontroler.php',
-                  params={'_action': 'home/plany/zmienNazwe', 'plan_id': plan_id},
-                  data=payload,
-                  headers={'Content-Type': 'multipart/form-data; boundary=' + boundary},
-                  cookies=cookies, timeout=20)
+    payload, boundary = create_form_str({
+        '_action': 'home/plany/zmienNazwe',
+        'plan_id': str(plan_id),
+        'csrftoken': csrftoken,
+        'nazwa': new_name
+    })
+    requests.post(
+        USOSWEB_KONTROLER,
+        params={'_action': 'home/plany/zmienNazwe', 'plan_id': plan_id},
+        data=payload,
+        headers={'Content-Type': 'multipart/form-data; boundary=' + boundary},
+        cookies=cookies, timeout=DEFAULT_TIMEOUT
+    )
 
 def get_all_plan_ids (cookies) -> list[int]:
     """Returns ids of all user plans."""
-    list_plans_request = requests.get ('https://usosweb.mimuw.edu.pl/kontroler.php',
-                                       params={'_action': 'home/plany/index'},
-                                       cookies=cookies, timeout=20)
+    list_plans_request = requests.get (
+        USOSWEB_KONTROLER,
+        params={'_action': 'home/plany/index'},
+        cookies=cookies,
+        timeout=DEFAULT_TIMEOUT
+    )
     return re.findall(r'data-plan-id="(\d*)"', list_plans_request.text)
 
 def copy_plan (plan_id: int, cookies):
     """Creates a copy of a plan."""
-    requests.get('https://usosweb.mimuw.edu.pl/kontroler.php',
-                 params={'_action': 'home/plany/skopiuj', 'plan_id': plan_id},
-                 cookies=cookies, timeout=20)
+    requests.get(
+        USOSWEB_KONTROLER,
+        params={'_action': 'home/plany/skopiuj', 'plan_id': plan_id},
+        cookies=cookies,
+        timeout=DEFAULT_TIMEOUT
+    )
 
 def duplicate_plan (plan_id: int, num: int, name: str, cookies) -> list[int]:
     """Duplicates the plan with given plan_id num times, numbers the duplicates
@@ -180,13 +204,15 @@ def get_entry_data (entry: bs4.element.Tag):
     name_match = typing.cast (re.Match[str], re.search (r'^([A-Z]*),\s*gr\.\s*(\d*)', name))
     time_match = typing.cast(re.Match[str], re.search(r'(\d*):(\d*) - (\d*):(\d*)', dates))
 
-    data = {'type': name_match.group(1),
-            'group_num': name_match.group(2),
-            'day': get_weekday_polish(dates),
-            'parity': parity_to_int_polish(get_parity_polish(dates)),
-            'subject': entry['name-id'],
-            'time_from': transform_time (time_match.group(1), time_match.group(2)),
-            'time_to': transform_time (time_match.group(3), time_match.group(4))}
+    data = {
+        'type': name_match.group(1),
+        'group_num': name_match.group(2),
+        'day': get_weekday_polish(dates),
+        'parity': parity_to_int_polish(get_parity_polish(dates)),
+        'subject': entry['name-id'],
+        'time_from': transform_time (time_match.group(1), time_match.group(2)),
+        'time_to': transform_time (time_match.group(3), time_match.group(4))
+    }
     return data
 
 @dataclass
@@ -272,21 +298,26 @@ def evaluate_plan_time (plan: list[GroupEntry], _: pathlib.Path) -> int:
     return res
 custom_evaluate_data = {}
 def evaluate_plan_custom (plan: list[GroupEntry], path: pathlib.Path) -> int:
+    """Returns plan badness as a sum of badnesses of each group in it."""
     if path in custom_evaluate_data:
         data = custom_evaluate_data[path]
     else:
-        with open ((path / 'data.json').resolve(), 'r') as data_file:
+        with open ((path / 'data.json').resolve(), 'r', encoding="utf-8") as data_file:
             data = json.load(data_file)
             custom_evaluate_data[path] = data
     result: int = 0
     for group in plan:
-        values_for_groups = [int(data[group.subject][group.entry_type][single_group]) for single_group in group.group_nums]
+        values_for_groups = [int(data[group.subject][group.entry_type][single_group])
+                             for single_group in group.group_nums]
         result += min(values_for_groups)
 
     return result
 
 
-evaluators = {'time': evaluate_plan_time, 'custom': evaluate_plan_custom}
+EVALUATORS = {
+    'time': evaluate_plan_time,
+    'custom': evaluate_plan_custom
+}
 
 CLASSTYPES = {
     "Laboratorium": 'LAB',
@@ -306,12 +337,11 @@ def shatter_course(plan_id: int, n: int, groups: dict[str, GroupEntry], cookies)
     Groups must be sorted by classtype into lists."""
     # groups: [classtype, GroupEntry]
     shatter_list_request = requests.get(
-        'https://usosweb.mimuw.edu.pl/kontroler.php',
+        USOSWEB_KONTROLER,
         params={'_action': 'home/plany/rozbijWpis', 'plan_id': plan_id, 'nr': n},
         cookies=cookies,
-        timeout=20
+        timeout=DEFAULT_TIMEOUT
     )
-    csrftoken: str = get_csrf_token(shatter_list_request.text)
     shatter_list_soup = BeautifulSoup(shatter_list_request.content, 'html.parser')
     # remaining groups
     remaining_indices: list[int] = []
@@ -335,18 +365,18 @@ def shatter_course(plan_id: int, n: int, groups: dict[str, GroupEntry], cookies)
         'plan_id': str(plan_id),
         'nr': str(n),
         'zapisz': '1',
-        'csrftoken': csrftoken
+        'csrftoken': get_csrf_token(shatter_list_request.text)
     }
     form_dict.update({'entry' + str(on_index): 'on' for on_index in remaining_indices})
 
     payload, boundary = create_form_str(form_dict)
     requests.post(
-        'https://usosweb.mimuw.edu.pl/kontroler.php',
+        USOSWEB_KONTROLER,
         params={'_action': 'home/plany/rozbijWpis', 'plan_id': plan_id},
         data=payload,
         headers={'Content-Type': 'multipart/form-data; boundary=' + boundary},
         cookies=cookies,
-        timeout=20
+        timeout=DEFAULT_TIMEOUT
     )
 
 def shatter_plan (plan_id: int, groups: dict[tuple[str, str], GroupEntry], cookies):
@@ -355,10 +385,10 @@ def shatter_plan (plan_id: int, groups: dict[tuple[str, str], GroupEntry], cooki
     # groups: [[course, classtype], groups]
 
     edit_request = requests.get (
-        'https://usosweb.mimuw.edu.pl/kontroler.php',
+        USOSWEB_KONTROLER,
         params={'_action': 'home/plany/edytuj', 'plan_id': plan_id},
         cookies=cookies,
-        timeout=20
+        timeout=DEFAULT_TIMEOUT
     )
     edit_soup = BeautifulSoup (edit_request.content, 'html.parser')
 
@@ -391,11 +421,12 @@ def merge_groups_by_time(groups: list[GroupEntry]) -> list[GroupEntry]:
 def get_groups_from_plan (plan_id: int, cookies) -> list[list[GroupEntry]]:
     """Returns all groups appearing in the plan,
     grouped in lists by their course units."""
-    plan_page = requests.get ('https://usosweb.mimuw.edu.pl/kontroler.php',
-                              params={'_action': 'home/plany/pokaz',
-                                      'plan_id': plan_id,
-                                      'plan_division': 'semester'},
-                              cookies=cookies, timeout=20)
+    plan_page = requests.get (
+        USOSWEB_KONTROLER,
+        params={'_action': 'home/plany/pokaz', 'plan_id': plan_id, 'plan_division': 'semester'},
+        cookies=cookies,
+        timeout=DEFAULT_TIMEOUT
+    )
 
     print ('downloaded plan')
 
@@ -478,7 +509,7 @@ def read_personal_config(path: pathlib.Path) -> (set[str], str):
     courses = set(read_words_from_file(str((path / 'codes').resolve())))
 
     words = read_words_from_file(str((path / 'eval').resolve()))
-    if len(words) == 1 and ((evaluator := words[0]) in evaluators):
+    if len(words) == 1 and ((evaluator := words[0]) in EVALUATORS):
         return courses, evaluator
     raise RuntimeError('Failed to read evaluator function')
 
@@ -504,12 +535,13 @@ def init_planner_unit_from_config(path: pathlib.Path,
 def get_top_timetables(planner_unit: PlannerUnit, n: int) -> list[(list[GroupEntry], int)]:
     """Returns top n timetables with scores for a given planner unit."""
     possible_plans = list_possible_plans(planner_unit.groups)
-    plans_with_values = [(plan, evaluators[planner_unit.evaluator](plan, planner_unit.config_path))
+    plans_with_values = [(plan, EVALUATORS[planner_unit.evaluator](plan, planner_unit.config_path))
                          for plan in possible_plans]
     # sort plans by badness, return top n
     return sorted(plans_with_values, key=lambda x: x[1])[:n]
 
-def main():
+def main() -> int:
+    """Calculates the best possible plans according to config and creates them in USOS."""
     dydactic_cycle: str = read_dydactic_cycle()
 
     current_hash = ''.join(random.choices('ABCDEFGH', k=6))
@@ -525,8 +557,12 @@ def main():
                         in config_directory.iterdir() if directory.is_dir()]
 
     for personal_config in personal_configs:
-        current_unit = init_planner_unit_from_config(personal_config, current_hash,
-                                                     dydactic_cycle, php_session_cookies)
+        current_unit = init_planner_unit_from_config(
+            personal_config,
+            current_hash,
+            dydactic_cycle,
+            php_session_cookies
+        )
         print(current_unit)
         all_planner_units.append(current_unit)
 
@@ -535,9 +571,13 @@ def main():
 
         # ids of copies of the original plan
         plan_instance_ids: list[int] = (
-            duplicate_plan(current_unit.template_plan_id, len(top_timetables),
-                           'automatic_instance_' + current_unit.name + '_' + current_hash + '__',
-                           php_session_cookies))
+            duplicate_plan(
+                current_unit.template_plan_id,
+                len(top_timetables),
+                'automatic_instance_' + current_unit.name + '_' + current_hash + '__',
+                php_session_cookies
+            )
+        )
 
         # recreate the top plans in USOS
         for (timetable, _), plan_id in zip(top_timetables, plan_instance_ids):
@@ -547,6 +587,7 @@ def main():
             }
             shatter_plan (plan_id, course_unit_to_groups, php_session_cookies)
             print ('shattered plan')
+    return 0
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
